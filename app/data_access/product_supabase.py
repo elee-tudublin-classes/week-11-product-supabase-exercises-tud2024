35958b8b25e81346012a007deabbf45a08fba78b
Tarak <x00204549@mytudublin.ie>
2024-12-07 19:04:18 +0000

    update lab11

--- app/data_access/product_supabase.py
+++ app/data_access/product_supabase.py
@@ -16,23 +16,36 @@ supabase: Client = create_client(db_url, db_key)
 # get all products
 def dataGetProducts():
     response = (supabase.table("product")
-                .select("*")
+                .select("*, category(name)")
                 .order("title", desc=False)
                 .execute()
     )
 
     return response.data
 
+def dataGetProductByCat(id:int):
+    response = (supabase.table("product")
+                .select("*, category(name)")
+                .eq("category_id",id)
+                .order("title", desc=False)
+                .execute()
+    )
+    return response.data
+
+    
+
 # get product by id
 def dataGetProduct(id):
     # select * from product where id = id 
     response = (
         supabase.table("product")
-        .select("*")
+        .select("*,category(name)")
         .eq("id", id)
         .execute()
     )
+
     return response.data[0]
+    
 
 # update product
 def dataUpdateProduct(product: Product) :
@@ -40,7 +53,7 @@ def dataUpdateProduct(product: Product) :
     #response = supabase.table("product").upsert({"id": product.id, "category_id": product.category_id, "title": product.title, "thumbnail": product.thumbnail, "stock": product.stock, "price": product.price}).execute()
     response = (
         supabase.table("product")
-        .upsert(product.dict()) # convert product object to dict - required by Supabase
+        .upsert(product.model_dump()) # convert product object to dict - required by Supabase
         .execute()
     )
     # result is 1st item in the list
@@ -48,13 +61,16 @@ def dataUpdateProduct(product: Product) :
 
 # add product, accepts product object
 def dataAddProduct(product: Product) :
+    
     response = (
         supabase.table("product")
-        .insert(product.dict()) # convert product object to dict - required by Supabase
+        .insert(product.model_dump()) # convert product object to dict - required by Supabase
         .execute()
     )
-    # result is 1st item in the list
-    return response.data[0]
+    
+    if (response.data):
+        return dataGetProduct(response.data[0]['id'])
+    return False
 
 # get all categories
 def dataGetCategories():
